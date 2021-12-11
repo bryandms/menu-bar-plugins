0d12ee31da2a81d7fde224df2a214d0cb27ed6b5
Bryan Morales <bryandms95@gmail.com>
2021-12-11 17:23:03 -0600

    MBP01-ShowOpenPRs | Add helpers

---
+++ src/helpers.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-undef */
+/* ––
+ * –––– Imports
+ * –––––––––––––––––––––––––––––––––– */
+const { exec } = require('child_process')
+const { ICON, PLUGIN_NAME } = require('./config')
+
+/* ––
+ * –––– Variables declaration
+ * –––––––––––––––––––––––––––––––––– */
+const command = 'defaults read -g AppleInterfaceStyle'
+
+/* ––
+ * –––– Public API
+ * –––––––––––––––––––––––––––––––––– */
+const loadOSTheme = new Promise((resolve) => {
+  exec(command, (error) => {
+    if (error) {
+      resolve('Light')
+    }
+    resolve('Dark')
+  })
+}).then((theme) => {
+  primaryColor = theme === 'Light' ? '#343a40' : '#f8f9fa'
+})
+
+const showPluginName = () => {
+  console.log(`${PLUGIN_NAME} | templateImage=${ICON}`)
+  console.log('---')
+}
+
+const showErrorMessage = () => {
+  console.log(':warning: ERROR')
+  console.log('---')
+  console.log('An error occurred while loading the plugin.')
+}
+
+/* ––
+ * –––– Exports
+ * –––––––––––––––––––––––––––––––––– */
+module.exports = {
+  loadOSTheme,
+  showPluginName,
+  showErrorMessage
+}
