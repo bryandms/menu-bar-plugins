e2c4326930601ab3d913926162b7245a279fb325
Bryan Morales <bryandms95@gmail.com>
2021-12-11 16:08:44 -0600

    MBP00-Inception | Add dotenv

---
+++ index.js
@@ -0,0 +1,9 @@
+/* ––
+ * –––– Imports
+ * –––––––––––––––––––––––––––––––––– */
+const path = require('path')
+const dotenv = require('dotenv')
+
+dotenv.config({
+  path: path.join(__dirname, '/.env')
+})
