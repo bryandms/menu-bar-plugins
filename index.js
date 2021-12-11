97af5296c77da1a74ee49ad4aeda287b225f5655
Bryan Morales <bryandms95@gmail.com>
2021-12-11 17:25:00 -0600

    MBP01-ShowOpenPRs | Add index file

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+/* eslint-disable no-unused-expressions */
 /* ––
  * –––– Imports
  * –––––––––––––––––––––––––––––––––– */
 const path = require('path')
-const dotenv = require('dotenv')
-
-dotenv.config({
+require('dotenv').config({
   path: path.join(__dirname, '/.env')
 })
+const helpers = require('./src/helpers')
+const modules = require('./src/modules')
+
+/* ––
+ * –––– Variables declaration
+ * –––––––––––––––––––––––––––––––––– */
+global.primaryColor = ''
+
+/* ––
+ * –––– Initializer declaration
+ * –––––––––––––––––––––––––––––––––– */
+helpers.loadOSTheme
+
+/* ––
+ * –––– Exports
+ * –––––––––––––––––––––––––––––––––– */
+module.exports = {
+  ...helpers,
+  ...modules
+}
